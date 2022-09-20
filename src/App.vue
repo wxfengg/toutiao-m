841b7631a8e4110082a3a8167dc842fc7618a5c3
wxf <3199257353@qq.com>
2022-09-20 11:02:21 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <h1>新闻头条</h1>
+    <div>
+      <span class="touxiao touxiao-shanchu"></span>
+    </div>
+    <div>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import store from './store'
 // 引入全局样式
 import './styles/index.less'
 
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
